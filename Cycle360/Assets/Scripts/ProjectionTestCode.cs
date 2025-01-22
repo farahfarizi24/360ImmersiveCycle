d9ff12f4e019628e15ddffeddf065ae5f292dcdc
farahfarizi24 <61811820+farahfarizi24@users.noreply.github.com>
2025-01-22 13:50:38 +1100

    Update

--- Cycle360/Assets/Scripts/ProjectionTestCode.cs
+++ Cycle360/Assets/Scripts/ProjectionTestCode.cs
@@ -5,16 +5,90 @@ using UnityEngine.Video;
 
 public class ProjectionTestCode : MonoBehaviour
 {
-    
+    public VideoPlayer ProjTestVP;
+    public VideoClip ProjPrac1;
+    public VideoClip[] ProjPrac2;
+    public VideoClip[] ProjPrac3;
+    public VideoClip[] ProjQ1;
+    public VideoClip[] ProjQ2;
+    public VideoClip[] ProjQ3;
+    public VideoClip[] ProjQ4;
+    public VideoClip[] ProjQ5;
+    public VideoClip[] ProjQ6;
+    public VideoClip[] ProjQ7;
+    public VideoClip[] ProjQ8;
+    public VideoClip[] ProjQ9;
+    public VideoClip[] ProjQ10;
+    public int CurrentVideoNumber; //13 video, 3 prac 10 final
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CurrentVideoNumber = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+    void StartTimer()
+    {
+        StartCoroutine(CountdownToStartVid());
+    }
+
+
+    IEnumerator CountdownToStartVid()
+    {
+        ProjTestVP.Play();
+        yield return new WaitForSeconds(1f);
+        ProjTestVP.Pause();
+        //start countdown
+        yield return new WaitForSeconds(3f);
+
+        
+    }
+    void RunCurrentVideo()
+    {
+        switch (CurrentVideoNumber) 
+        {
+            case 1:
+                ProjTestVP.clip = ProjPrac1;
+                break;
+            case 2:
+                break;
+
+            case 3:
+                break;
+                case 4:
+                break;
+                case 5:
+                break;
+                case 6:
+                break;
+                case 7:
+                break;
+                case 8:
+                break;
+                case 9:
+                break;
+                case 10:
+                break;
+                case 11:
+                break;
+                case 12:
+                break;
+                case 13:
+                break;
+
+
+
+
+        }
+
+
     }
 }
