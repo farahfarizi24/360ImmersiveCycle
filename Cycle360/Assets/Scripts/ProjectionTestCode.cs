f893803395b7ffa20753e5b826209f091bc9f35c
farahfarizi24 <61811820+farahfarizi24@users.noreply.github.com>
2025-01-22 16:06:33 +1100

    Update some code
    Need to make sure the clips are changing from one to another

--- Cycle360/Assets/Scripts/ProjectionTestCode.cs
+++ Cycle360/Assets/Scripts/ProjectionTestCode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,25 +21,82 @@ public class ProjectionTestCode : MonoBehaviour
     public VideoClip[] ProjQ9;
     public VideoClip[] ProjQ10;
     public int CurrentVideoNumber; //13 video, 3 prac 10 final
-
+    public int SplittedClipNumber;
+    private int TotalVideo = 13;
+    public int TotalClipForThisVideo;
+    public bool isChanging=false;
     // Start is called before the first frame update
     void Start()
     {
+        SplittedClipNumber = 0;
         CurrentVideoNumber = 1;
+        RunCurrentVideo();
+        StartCoroutine(CountdownToStartVid());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProjTestVP.loopPointReached += DecideWhatToDoNext;
+
     }
 
-
-
-    void StartTimer()
+    private void DecideWhatToDoNext(VideoPlayer source)
     {
-        StartCoroutine(CountdownToStartVid());
+        if (!isChanging)
+        {
+            isChanging = true;
+            if (CurrentVideoNumber == 1)
+            {
+                CurrentVideoNumber++;
+                ProjTestVP.Stop();
+
+                RunCurrentVideo();
+                StartCoroutine(CountdownToStartVid());
+            }
+            else
+            {
+                if (CurrentVideoNumber != TotalVideo)
+                {
+                    if (SplittedClipNumber <= TotalClipForThisVideo-1)
+                    {
+                        ProjTestVP.Stop();
+
+                        SplittedClipNumber++;
+                        Debug.Log(SplittedClipNumber.ToString());
+                        RunCurrentVideo();
+                        ProjTestVP.Play();
+                       
+                        //StartCoroutine(CountdownToStartVid());
+                    }
+                    else
+                    {
+                        ProjTestVP.Stop();
+
+                        SplittedClipNumber = 0;
+                        CurrentVideoNumber++;
+                        RunCurrentVideo();
+                        StartCoroutine(CountdownToStartVid());
+                    }
+
+
+                    //check which is currently active;
+                    //add split clip number if they're not at the end
+                    //add video number 
+
+                }
+                else
+                {
+                    //EndingScene
+                }
+
+            }
+        }
+      
     }
+
+  
 
 
     IEnumerator CountdownToStartVid()
@@ -48,40 +106,82 @@ public class ProjectionTestCode : MonoBehaviour
         ProjTestVP.Pause();
         //start countdown
         yield return new WaitForSeconds(3f);
+        ProjTestVP.Play();
+        isChanging = false;
 
-        
     }
     void RunCurrentVideo()
     {
+
         switch (CurrentVideoNumber) 
         {
             case 1:
+               
+
                 ProjTestVP.clip = ProjPrac1;
                 break;
             case 2:
+                ProjTestVP.clip = ProjPrac2[SplittedClipNumber];
+                TotalClipForThisVideo = ProjPrac2.Length;
+                
                 break;
 
             case 3:
+                ProjTestVP.clip = ProjPrac3[SplittedClipNumber];
+                TotalClipForThisVideo = ProjPrac3.Length;
+
+
+
                 break;
                 case 4:
+                ProjTestVP.clip = ProjQ1[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ1.Length;
+
                 break;
                 case 5:
+                ProjTestVP.clip = ProjQ2[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ2.Length;
+
                 break;
                 case 6:
+                ProjTestVP.clip = ProjQ3[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ3.Length;
+
                 break;
                 case 7:
+                ProjTestVP.clip = ProjQ4[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ4.Length;
+
                 break;
                 case 8:
+                ProjTestVP.clip = ProjQ5[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ5.Length;
+
                 break;
                 case 9:
+                ProjTestVP.clip = ProjQ6[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ6.Length;
+
                 break;
                 case 10:
+                ProjTestVP.clip = ProjQ7[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ7.Length;
+
                 break;
                 case 11:
+                ProjTestVP.clip = ProjQ8[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ8.Length;
+
                 break;
                 case 12:
+                ProjTestVP.clip = ProjQ9[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ9.Length;
+
                 break;
                 case 13:
+                ProjTestVP.clip = ProjQ10[SplittedClipNumber];
+                TotalClipForThisVideo = ProjQ10.Length;
+
                 break;
 
 
